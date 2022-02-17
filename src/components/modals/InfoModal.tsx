212612359ca10f431b3a704b24d1bc1c73fb26d5
Shota Miyazaki <jmsf0203@gmail.com>
2022-02-17 10:36:27 +0900

    Update help
    help 更新。
    red, green, mediumblue にした。それぞれの位置わかったほうが良さそうなため。

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,62 +10,81 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the word in 6 tries. After each guess, the color of the tiles will
-        change to show how close your guess was to the word.
+        Guess the color of the tile above in 6 tries. After each guess, the
+        color of the tiles will change to show how close your guess was to the
+        color. And the color of the characters will be the color your guess.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="W"
+          value="F"
           status="correct"
+          color="FF0000"
         />
-        <Cell value="E" />
-        <Cell value="A" />
-        <Cell value="R" />
-        <Cell value="Y" />
+        <Cell value="F" color="FF0000" />
+        <Cell value="0" color="FF0000" />
+        <Cell value="0" color="FF0000" />
+        <Cell value="0" color="FF0000" />
+        <Cell value="0" color="FF0000" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
+        The letter F is in the color code and in the correct spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
-        <Cell value="I" />
+        <Cell value="0" color="008000" />
+        <Cell value="0" color="008000" />
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="L"
+          value="8"
+          color="008000"
           status="present"
         />
-        <Cell value="O" />
-        <Cell value="T" />
+        <Cell value="0" color="008000" />
+        <Cell value="0" color="008000" />
+        <Cell value="0" color="008000" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
+        The letter 8 is in the color code but in the wrong spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
-        <Cell value="A" />
-        <Cell value="G" />
-        <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
-        <Cell value="E" />
+        <Cell value="0" color="0000CD" />
+        <Cell value="0" color="0000CD" />
+        <Cell value="0" color="0000CD" />
+        <Cell value="0" color="0000CD" />
+        <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="C"
+          color="0000CD"
+          status="absent"
+        />
+        <Cell value="D" color="0000CD" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        The letter U is not in the color code in any spot.
       </p>
 
+      <p className="mt-6 text-sm text-gray-500 dark:text-gray-300">
+        Colorfle by{' '}
+        <a href="https://twitter.com/mazamachi" className="underline font-bold">
+          @mazamachi
+        </a>
+        .
+      </p>
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
-        love -{' '}
+        Forked from{' '}
         <a
           href="https://github.com/cwackerfuss/react-wordle"
           className="underline font-bold"
         >
-          check out the code here
-        </a>{' '}
+          react-wordle
+        </a>
+        .
       </p>
     </BaseModal>
   )
